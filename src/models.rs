f04098840f5dd40a5a45fd9e749aec7ab4ca9c97
Owen Nelson <onelson@gmail.com>
2017-08-08 23:37:18 -0700

    fleshing out db stuffs

--- src/models.rs
+++ src/models.rs
@@ -1,20 +1,21 @@
-#[derive(Debug)]
+#[derive(Debug, Queryable)]
 pub struct Zone {
     id: u32,
-    name: String
+    name: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Queryable)]
 pub struct Category {
     id: u32,
-    name: String
+    name: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Queryable)]
 pub struct Collectible {
     id: u32,
     name: String,
     description: Option<String>,
     category: u32,
-    zones: Vec<u32>
+    zones: Vec<u32>,
+    got_it: bool
 }
