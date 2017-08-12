db49adff4c39f23efe30aca557c3f659a479dd55
Owen Nelson <onelson@gmail.com>
2017-08-11 22:26:49 -0700

    add data migration for item categories

---
+++ migrations/20170812050609_categories_data/up.sql
@@ -0,0 +1,10 @@
+INSERT INTO categories (id, name) VALUES
+  (1, "Snakes"),
+  (2, "Birds"),
+  (3, "Medical Plants"),
+  (4, "Frogs"),
+  (5, "Fish"),
+  (6, "Mushrooms"),
+  (7, "Fruit"),
+  (8, "Misc")
+;
