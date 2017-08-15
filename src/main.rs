4ff29bea19e080cc7821c81f92b8f2e1d4b94b95
Owen Nelson <onelson@gmail.com>
2017-08-14 21:40:03 -0700

    add all list endpoints

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ extern crate diesel;
 extern crate diesel_codegen;
 extern crate dotenv;
 extern crate env_logger;
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 extern crate r2d2;
 extern crate r2d2_diesel;
 extern crate rocket;
@@ -26,20 +27,57 @@ mod schema {
 
 mod db;
 mod models;
+
+
 use std::env;
-use rocket_contrib::Json;
-use diesel::result::QueryResult;
-use diesel::prelude::*;
+mod api {
+    use rocket_contrib::Json;
+    use diesel::result::QueryResult;
+    use diesel::prelude::*;
+    use db;
+    use models;
+
+    #[get("/zones")]
+    pub fn get_zones(conn_: db::Conn) -> QueryResult<Json<Vec<models::Zone>>> {
+        use schema::zones::dsl::*;
+        let ref conn = *conn_;
+        zones
+            .order(name.asc())
+            .load::<models::Zone>(conn)
+            .map(|xs| Json(xs))
+    }
 
 
+    #[get("/categories")]
+    pub fn get_categories(conn_: db::Conn) -> QueryResult<Json<Vec<models::Category>>> {
+        use schema::categories::dsl::*;
+        let ref conn = *conn_;
+        categories
+            .order(name.asc())
+            .load::<models::Category>(conn)
+            .map(|xs| Json(xs))
+    }
 
-#[get("/zones")]
-fn get_zones(conn_: db::Conn) -> QueryResult<Json<Vec<models::Zone>>> {
-    use schema::zones::dsl::*;
-    let ref conn = *conn_;
-    zones
-        .order(name.desc())
-        .load::<models::Zone>(conn).map(|xs| Json(xs))
+    #[get("/collectibles")]
+    pub fn get_collectibles(conn_: db::Conn) -> QueryResult<Json<Vec<models::Collectible>>> {
+        use schema::collectibles::dsl::*;
+        let ref conn = *conn_;
+        collectibles
+            .order(name.asc())
+            .load::<models::Collectible>(conn)
+            .map(|xs| Json(xs))
+    }
+
+    #[get("/membership")]
+    pub fn get_memberships(
+        conn_: db::Conn,
+    ) -> QueryResult<Json<Vec<models::CollectibleZoneMembership>>> {
+        use schema::collectible_zone_membership::dsl::*;
+        let ref conn = *conn_;
+        collectible_zone_membership
+            .load::<models::CollectibleZoneMembership>(conn)
+            .map(|xs| Json(xs))
+    }
 }
 
 
@@ -50,6 +88,14 @@ fn main() {
     debug!("using db url: {}", database_url);
     rocket::ignite()
         .manage(db::init_pool(&database_url))
-        .mount("/", routes![get_zones])
+        .mount(
+            "/api",
+            routes![
+                api::get_zones,
+                api::get_categories,
+                api::get_collectibles,
+                api::get_memberships
+            ],
+        )
         .launch();
 }
