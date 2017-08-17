91b85d0ac0eb517792c3ebb7b26a8967af95bcff
Owen Nelson <onelson@gmail.com>
2017-08-17 15:52:43 -0700

    no need to add an appearance column - just use description.

--- migrations/20170809050929_create_collectibles_table/up.sql
+++ migrations/20170809050929_create_collectibles_table/up.sql
@@ -3,7 +3,6 @@ CREATE TABLE collectibles (
   short_name TEXT,
   name TEXT UNIQUE NOT NULL,
   description TEXT,
-  appearance TEXT,
   category INTEGER NOT NULL,
   collected BOOLEAN NOT NULL,
   FOREIGN KEY(category) REFERENCES categories(id)
