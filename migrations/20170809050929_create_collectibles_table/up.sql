58993669aafc844f3864a406dcf2cf30e24fed78
Owen Nelson <onelson@gmail.com>
2017-08-17 16:01:52 -0700

    reorder fields to match the struct

--- migrations/20170809050929_create_collectibles_table/up.sql
+++ migrations/20170809050929_create_collectibles_table/up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE collectibles (
   id INTEGER PRIMARY KEY NOT NULL,
-  short_name TEXT,
   name TEXT UNIQUE NOT NULL,
+  short_name TEXT,
   description TEXT,
   category INTEGER NOT NULL,
   collected BOOLEAN NOT NULL,
