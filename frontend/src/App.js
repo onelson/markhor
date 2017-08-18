3a369411c22c5c8bca083fcc89ce7e53412833a8
Owen Nelson <onelson@gmail.com>
2017-08-17 17:52:18 -0700

    Implemented filtering by zone on CollectibleList, and subscribed to store.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-          <div>
+          <div className="app">
               <ZonePicker/>
               <CollectibleList/>
           </div>
--- frontend/src/CollectibleList.js
+++ frontend/src/CollectibleList.js
@@ -1,15 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import * as Immutable from 'immutable';
 
 CollectibleList.propTypes = {
-    items: PropTypes.object.isRequired
+    activeZone: PropTypes.number,
+    collectibles: PropTypes.object.isRequired,
+    categories: PropTypes.object.isRequired,
+    membership: PropTypes.object.isRequired
+
 };
 
-export default function CollectibleList(props) {
-    return (<ul>
-        <li>items</li>
-        <li>items</li>
-        <li>items</li>
-        <li>items</li>
-    </ul>);
+function mapStateToProps(state) {
+    return {
+        activeZone: state.activeZone,
+        collectibles: state.collectibles,
+        categories: state.categories,
+        membership: state.membership
+    };
 }
+
+function CollectibleList(props) {
+    let items = Immutable.List();
+
+    if (props.activeZone === null) {
+        items = props.collectibles.valueSeq();
+    }
+    else if (props.membership.itemsByZone.has(props.activeZone)) {
+        const validIds = props.membership.itemsByZone.get(props.activeZone);
+        items = props
+            .collectibles
+            .valueSeq()
+            .filter(x => validIds.includes(x.id)).toList();
+    }
+
+    if (items.size > 0) {
+        items = items
+            .sortBy(x => [x.category, x.name])
+            .map(item => <li key={item.id} className="item">{item.name}</li>);
+    }
+    else {
+        items = <li>No items found in this zone.</li>;
+    }
+
+    return (
+        <ul className="collectible-list">
+            {items}
+        </ul>);
+}
+
+export default connect(mapStateToProps)(CollectibleList);
