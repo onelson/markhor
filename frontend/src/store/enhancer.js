0ab628c47848f0927e26f52b073b049118e2de4d
Owen Nelson <onelson@gmail.com>
2024-01-06 22:39:01 -0800

    chore: remove react frontend
    The old frontend code was just here for reference during the transition
    to Yew.

--- frontend/src/store/enhancer.js
+++
@@ -1,9 +0,0 @@
-import { applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-
-// Middleware for the redux store used in prod/dev (both).
-export default applyMiddleware(
-    thunk,
-    // If more middlewares are needed, just pass
-    // them in here as additional args.
-    );
